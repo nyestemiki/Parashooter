8b7f3367d0560f28a81326f6e05dcada35df389a
Miki Nyeste <nyeste.miki@yahoo.com>
2019-07-30 00:32:39 +0300

    Added Gitignore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,13 @@ public class GameManager : Singleton<GameManager> {
     [SerializeField] private GameObject ground;
     [SerializeField] private GameObject player;
     [SerializeField] private Text displayDistanceLeftWindow;
+    [SerializeField] private GameObject startScreen;
     private float currentHeight;
     private float mass; 
     private float drag; 
     private float angularDrag;
     private Rigidbody rigidbody;
+    private bool isStarted = false;
 
     void Start() {
         mass = 10f;
@@ -20,6 +22,7 @@ public class GameManager : Singleton<GameManager> {
         angularDrag = 0.05f;
 
         rigidbody = player.GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
         
         rigidbody.mass = mass;
         rigidbody.drag = drag;
@@ -27,14 +30,29 @@ public class GameManager : Singleton<GameManager> {
     }
 
     void Update() {
+        if (!isStarted) {
+            return;
+        }
+
         // rigidbody.mass = mass;
         // rigidbody.drag = drag;
         // rigidbody.angularDrag = angularDrag;
+
         currentHeight = Mathf.Abs(transform.position.y - ground.transform.position.y); 
         displayDistanceLeftWindow.text = currentHeight.ToString("0");
 
         if (Input.GetMouseButtonDown(0)) {
-            rigidbody.isKinematic = true;
+            ActivateParashoot();
         }
+    }
+
+    public void Play() {
+        isStarted = true;
+        startScreen.SetActive(false);
+        rigidbody.isKinematic = false;
+    }
+
+    public void ActivateParashoot() {
+        rigidbody.isKinematic = true;
     }
 }
