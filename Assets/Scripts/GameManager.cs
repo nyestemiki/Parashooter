1f44b432ba98bc23754f5a98f2be951a30c5d638
Miki Nyeste <nyeste.miki@yahoo.com>
2019-07-31 23:32:04 +0300

    Resolving hard coded values

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,23 +10,19 @@ public class GameManager : Singleton<GameManager> {
     [SerializeField] private Text displayDistanceLeftWindow;
     [SerializeField] private GameObject startScreen;
     private float currentHeight;
-    private float mass; 
-    private float drag; 
-    private float angularDrag;
     private Rigidbody rigidbody;
+    private float hideBorder;
     private bool isStarted = false;
 
     void Start() {
-        mass = 10f;
-        drag = 0f;
-        angularDrag = 0.05f;
-
         rigidbody = player.GetComponent<Rigidbody>();
         rigidbody.isKinematic = true;
         
-        rigidbody.mass = mass;
-        rigidbody.drag = drag;
-        rigidbody.angularDrag = angularDrag;
+        rigidbody.mass = GetRandomMass();
+        rigidbody.drag = GetRandomDrag();
+        rigidbody.angularDrag = GetRandomAngularDrag();
+
+        hideBorder = GetRandomHideDistanceDisplay();
 
         currentHeight = Mathf.Abs(transform.position.y - ground.transform.position.y); 
     }
@@ -36,18 +32,12 @@ public class GameManager : Singleton<GameManager> {
             return;
         }
 
-        // rigidbody.mass = mass;
-        // rigidbody.drag = drag;
-        // rigidbody.angularDrag = angularDrag;
-
-        
-
-        if (currentHeight < 200) {
-            displayDistanceLeftWindow.text = "";
-        } else {
+        if (currentHeight > hideBorder) {
             currentHeight = Mathf.Abs(transform.position.y - ground.transform.position.y); 
             displayDistanceLeftWindow.text = currentHeight.ToString("0");
-        }
+        } else {
+            displayDistanceLeftWindow.text = "";
+        }  
 
         if (Input.GetMouseButtonDown(0)) {
             ActivateParashoot();
@@ -62,5 +52,21 @@ public class GameManager : Singleton<GameManager> {
 
     public void ActivateParashoot() {
         rigidbody.isKinematic = true;
+    }
+
+    private float GetRandomMass() {
+        return new Random().Next(50, 150);
+    }
+
+    private float GetRandomDrag() {
+        return new Random().Next(0, 10);
+    }
+
+    private float GetRandomAngularDrag() {
+        return new Random().Next(0.05, 1);
+    }
+
+    private float GetRandomHideDistanceDisplay() {
+        return new Random().Next(100, 500);
     }
 }
