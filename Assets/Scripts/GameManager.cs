ee0c3263945c9c5d15896d086cccc62162b9fefa
Miki Nyeste <nyeste.miki@yahoo.com>
2019-12-08 02:20:10 +0200

    Version 1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,26 +11,47 @@ public class GameManager : Singleton<GameManager> {
     [SerializeField] private Text displayDistanceLeftWindow;
     [SerializeField] private GameObject startScreen;
     [SerializeField] private GameObject lostScreen;
+    [SerializeField] private GameObject stoppedScreen;
+    [SerializeField] private Text stoppedDisplay;
     [SerializeField] private float currentHeight;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip idle;
+    [SerializeField] private AudioClip falling;
+    private float startPosition = 39f;
     private Rigidbody rigidbody;
     private float hideBorder;
     private bool isStarted = false;
 
     void Awake() {
+        rigidbody = player.GetComponent<Rigidbody>();
+        setInitialHeight();
+
+        startScreen.SetActive(true);
+
         lostScreen.SetActive(false);
+        stoppedScreen.SetActive(false);
     }
 
     void Start() {
-        rigidbody = player.GetComponent<Rigidbody>();
         rigidbody.isKinematic = true;
-        
+        randomInitialData();
+        setCurrentHeight();
+    }
+
+    private void randomInitialData() {
         rigidbody.mass = GetRandomMass();
         rigidbody.drag = GetRandomDrag();
         rigidbody.angularDrag = GetRandomAngularDrag();
 
         hideBorder = GetRandomHideDistanceDisplay();
+    }
 
+    private void setCurrentHeight() {
         currentHeight = Mathf.Abs(transform.position.y - ground.transform.position.y); 
+    }
+
+    private void setInitialHeight() {
+        player.transform.position = new Vector3(transform.position.x, startPosition, transform.position.z);
     }
 
     void Update() {
@@ -50,19 +71,48 @@ public class GameManager : Singleton<GameManager> {
             ActivateParashoot();
         }
 
-        if (currentHeight < 2) {
+        if (currentHeight < 50) {
+            rigidbody.isKinematic = true;
             Lost();
         }
     }
 
     public void Play() {
-        isStarted = true;
-        startScreen.SetActive(false);
         rigidbody.isKinematic = false;
+        isStarted = true;
+        audioSource.clip = falling;
+        audioSource.Play();
+        startScreen.SetActive(false);
+    }
+
+    public void Replay() {
+        audioSource.clip = idle;
+        audioSource.Play();
+        isStarted = false;
+        startScreen.SetActive(true);
+        lostScreen.SetActive(false);
+        stoppedScreen.SetActive(false);
+        displayDistanceLeftWindow.text = "";
+        randomInitialData();
+        setInitialHeight();
     }
 
     public void ActivateParashoot() {
+        audioSource.clip = idle;
+        audioSource.Play();
         rigidbody.isKinematic = true;
+        stoppedScreen.SetActive(true);
+        isStarted = false;
+        
+        if (currentHeight > 300) {
+            stoppedDisplay.text = "Too soon";
+        } else if (currentHeight > 200) {
+            stoppedDisplay.text = "Okay...ish";
+        } else if (currentHeight > 100) {
+            stoppedDisplay.text = "Good job";
+        } else {
+            stoppedDisplay.text = "Impressive";
+        }
     }
 
     private float GetRandomMass() {
@@ -83,5 +133,8 @@ public class GameManager : Singleton<GameManager> {
 
     private void Lost() {
         lostScreen.SetActive(true);
+        audioSource.clip = idle;
+        audioSource.Play();
+        isStarted = false;
     }
 }
